c08178dd8a8d3b4dd69cc54eaa1fee1f04305080
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-07-02 13:03:08 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Skills from './pages/Skills';
+import Education from './pages/Education';
+import Achievements from './pages/Achievements';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 
@@ -15,6 +17,8 @@ function App({ themeMode, toggleTheme }) {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/education" element={<Education />} />
+  <Route path="/achievements" element={<Achievements />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
